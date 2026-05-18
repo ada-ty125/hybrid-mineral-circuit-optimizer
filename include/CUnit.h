a3e5ca0d00d7527bd161c9b48f6957c01a40d942
ada-jjw25 <julia.williams25@imperial.ac.uk>
2026-05-18 15:58:07 +0100

    FEAT: add CUnit.h file with variables and functions

--- include/CUnit.h
+++ include/CUnit.h
@@ -5,6 +5,11 @@
 
 #pragma once
 
+#include <vector>
+#include <array>
+
+
+constexpr int N_COMPONENTS = 3
 
 class CUnit {
 
@@ -15,17 +20,48 @@ class CUnit {
   //A Boolean that is changed to true if the unit has been seen 
   bool mark;
 
-  int* output;
+  // Output from this unit where [0] = Palusznium, [1] = Gormanium, [2] = Waste
+  std::vector<int> output;
 
+  // Current feed entering this unit
+  // [0] = Palusznium
+  // [1] = Gormanium
+  // [2] = Waste
+  double feed[N_COMPONENTS] = {0.0, 0.0, 0.0};
 
+  // Feed from previous iteration
+  double old_feed[N_COMPONENTS] = {0.0, 0.0, 0.0};
 
-  CUnit();
-  ~CUnit();
+  // Concentrate output streams
+  // concentrate[i][j]
+  // i = output stream number
+  // j = component
+  std::vector<std::array<double, N_COMPONENTS>> concentrate;
 
-  /*
+  // Tailings stream
+  double tails[N_COMPONENTS] = {0.0, 0.0, 0.0};
 
-    ...other member functions and variables of CUnit
+  // Used for validity checking
+  bool mark = false;
 
-  */
+  // Constants for calculations
+  double volume = 10.0;
+  double phi = 0.1;
+  double rho = 3000.0;
+
+  // Recovery constants
+  // k[0] = Palusznium
+  // k[1] = Gormanium
+  // k[2] = Waste
+  double k[N_COMPONENTS] = {0.0, 0.0, 0.0};
+
+  // Simulation functions
+  double calculate_residence_time() const;
+
+  double calculate_recovery(int component) const;
+
+  void calculate_outputs();
+
+  void clear_feeds();
 };
 
